3f713d043b030db7e698ef06f98c6099c73a3d28
Andres Hurtado <AHurtado.WC@gmail.com>
2020-11-28 03:36:49 -0400

    add some replyes in Spanish

---
+++ lang/es.js
@@ -0,0 +1,12 @@
+const ERROR =
+  "\nSi estas experimentando algun error, por favor dejamelo saber escribiendo a @OsiNubis99";
+module.exports = {
+  group_added: "Grupo agregado",
+  is_not_a_group:
+    "Este comando solo esta disponible para su uso en grupos." + ERROR,
+  no_admin_person: "No pareces ser un administrador del Bot...",
+  remember_reply: "Recuerda responder al mensaje del otro jugador",
+  user_added: "Usuario agregado",
+  user_banned: "Usuario Bloqueado",
+  user_unbanned: "Usuario Desbloqueado",
+};
