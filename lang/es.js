75ace7b5bb1934687a43f74b98c4f9df2f7128f8
Andres Hurtado <AHurtado.WC@gmail.com>
2020-11-28 18:41:35 -0400

    feat: add some replyes

--- lang/es.js
+++ lang/es.js
@@ -1,12 +1,14 @@
 const ERROR =
   "\nSi estas experimentando algun error, por favor dejamelo saber escribiendo a @OsiNubis99";
 module.exports = {
-  group_added: "Grupo agregado",
+  group_added: "Grupo agregado.",
   is_not_a_group:
     "Este comando solo esta disponible para su uso en grupos." + ERROR,
-  no_admin_person: "No pareces ser un administrador del Bot...",
-  remember_reply: "Recuerda responder al mensaje del otro jugador",
-  user_added: "Usuario agregado",
-  user_banned: "Usuario Bloqueado",
-  user_unbanned: "Usuario Desbloqueado",
+  no_admin_person: "No pareces ser un administrador del Bot.",
+  no_ban_groups: "No es posible Bloquear un Bot.",
+  no_unban_groups: "No es posible Desbloquear un Bot.",
+  remember_reply: "Recuerda responder al mensaje del otro jugador.",
+  user_added: "Usuario agregado.",
+  user_banned: "Usuario Bloqueado.",
+  user_unbanned: "Usuario Desbloqueado.",
 };
