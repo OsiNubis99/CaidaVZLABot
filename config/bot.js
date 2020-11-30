1806eb1c6e6344bab345d23d88d30bfd4a7d6f8c
Andres Hurtado <AHurtado.WC@gmail.com>
2020-11-30 02:33:43 -0400

    fix: Change configs folder into config

--- config/bot.js
+++ config/bot.js
@@ -1,5 +1,5 @@
-const env = require("./env");
 const TelegramBot = require("node-telegram-bot-api");
+const env = require("./env");
 
 let options = {};
 if (env.NODE_ENV == "develop") {
@@ -12,13 +12,16 @@ const bot = new TelegramBot(env.TOKEN, options);
 
 if (env.NODE_ENV == "production") bot.setWebHook(`${env.url}/bot${env.TOKEN}`);
 
-bot.onText(/\/version/, (msg) => {
-  bot.sendMessage(msg.chat.id, version);
+bot.onText(/\/version(.*)/, (msg) => {
+  bot.sendMessage(msg.chat.id, env.name + "@" + env.version);
 });
 
-bot.onText(/\/saluda (.+)/, (msg, match) => {
+bot.onText(/\/saluda(.*)/, (msg, match) => {
   let chatId = msg.chat.id;
-  let resp = "Hola @" + msg.from.username + "\nTu mensaje fue: " + match[1];
+  let resp =
+    "Hola " +
+    (msg.from.username ? "@" + msg.from.username : msg.from.first_name) +
+    (match[1] ? "\nTu mensaje fue: " + match[1] : "");
   bot.sendMessage(chatId, resp);
 });
 
