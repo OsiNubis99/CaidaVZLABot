1e2f0ee4a1fd3f0a519842da0d4aff92d4433e94
Andres Hurtado <OsiNubis99@PM.me>
2021-03-18 12:05:04 -0400

    fix: Constructor created

--- class/Factory_User.js
+++ class/Factory_User.js
@@ -1,4 +1,5 @@
 const TelegramBot = require("node-telegram-bot-api");
+const resp = require("../lang/es");
 
 class Factory_User {
   id_user = String.prototype;
@@ -12,17 +13,26 @@ class Factory_User {
   caido = Number.prototype;
   is_banned = Boolean.prototype;
 
+  constructor(id_user, first_name, last_name, username) {
+    this.id_user = id_user;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.username = username;
+  }
+
   /**
    * @param {TelegramBot.User} telegramUser
-   * @returns A User element
+   * @returns {Factory_User|false} A User element
    */
   static fromTelegram(telegramUser) {
-    return {
-      id_user: telegramUser.id.toString(),
-      first_name: telegramUser.first_name,
-      last_name: telegramUser.last_name,
-      username: telegramUser.username,
-    };
+    if (telegramUser) {
+      let id_user = telegramUser.id.toString();
+      let first_name = telegramUser.first_name;
+      let last_name = telegramUser.last_name || "";
+      let username = telegramUser.username || null;
+      return new Factory_User(id_user, first_name, last_name, username);
+    }
+    return false;
   }
 }
 module.exports = Factory_User;
