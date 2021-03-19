a2dbbfe65e951d7a256fd995335ce29821ed798f
Andres Hurtado <OsiNubis99@PM.me>
2021-03-18 20:20:22 -0400

    feat: Delete Admin and run stable game middleware

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const bot = require("./config/server");
 const game = require("./middleware/game");
 const admin = require("./middleware/admin");
 const keyboard = require("./templates/keyboard");
-const user = require("./controller/user");
+const Factory_Request = require("./class/Factory_Request");
 
-//**                    InLine Querys                    */
+//**                    InLine Query                    */
 
 let inline_commands = ["points", "mode", "caida", "ronda", "cantos"];
 
@@ -41,115 +41,144 @@ bot.on("chosen_inline_result", (query) => {
 bot.on("callback_query", async (query) => {
   let response = "";
   bot.answerCallbackQuery(query.id);
-  switch (query.data) {
-    case "how_config":
-      bot.editMessageText(resp.how_config, {
-        reply_markup: keyboard.back,
-        chat_id: query.message.chat.id,
-        message_id: query.message.message_id,
-      });
-      break;
-    case "back":
-      response = await game.config(query.message, true);
-      bot.editMessageText(response.message, response.options);
-      break;
-    case "type":
-      response = await game.set_inline_type(query.message);
-      if (response) bot.editMessageText(response.message, response.options);
-      break;
-    default:
-      bot.deleteMessage(query.message.chat.id, query.message.message_id);
-      break;
+  if (query.data.match(/set_(.*)/)) {
+    let game_mode = parseInt(query.data.match(/set_(.*)/)[1]);
+    response = await game.set_inline_game_mode(query.message, game_mode);
+    if (response) bot.editMessageText(response.message, response.options);
+  } else {
+    switch (query.data) {
+      case "how_config":
+        bot.editMessageText(resp.how_config, {
+          reply_markup: keyboard.back,
+          chat_id: query.message.chat.id,
+          message_id: query.message.message_id,
+        });
+        break;
+      case "back":
+        response = await game.config(
+          Factory_Request.fromTelegram(query.message),
+          true
+        );
+        bot.editMessageText(response.message, response.options);
+        break;
+      case "type":
+        response = await game.set_inline_type(
+          Factory_Request.fromTelegram(query.message)
+        );
+        bot.editMessageText(response.message, response.options);
+        break;
+      case "run":
+        response = await game.shuffle(
+          Factory_Request.fromTelegram(query.message)
+        );
+        if (response) {
+          bot.editMessageText(response.message, response.options);
+          break;
+        }
+      default:
+        bot.deleteMessage(query.message.chat.id, query.message.message_id);
+        break;
+    }
   }
 });
 
 //**                   Admins Commands                   */
+//TODO all Admin module
 
-bot.onText(/\/addGroup/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, admin.add_group(msg), {
-    reply_to_message_id: msg.message_id,
-  });
-});
+// bot.onText(/\/addGroup/, (msg, match) => {
+//   bot.sendMessage(msg.chat.id, admin.add_group(msg), {
+//     reply_to_message_id: msg.message_id,
+//   });
+// });
 
-bot.onText(/\/bloquear/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, admin.ban_user(msg), {
-    reply_to_message_id: msg.message_id,
-  });
-});
+// bot.onText(/\/lock/, (msg, match) => {
+//   bot.sendMessage(msg.chat.id, admin.ban_user(msg), {
+//     reply_to_message_id: msg.message_id,
+//   });
+// });
 
-bot.onText(/\/desbloquear/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, admin.unban_user(msg), {
-    reply_to_message_id: msg.message_id,
-  });
-});
+// bot.onText(/\/unlock/, (msg, match) => {
+//   bot.sendMessage(msg.chat.id, admin.unban_user(msg), {
+//     reply_to_message_id: msg.message_id,
+//   });
+// });
 
-bot.onText(/\/listGroups/, async (msg, match) => {
-  bot.sendMessage(msg.chat.id, await admin.list_group(msg), {
-    reply_to_message_id: msg.message_id,
-  });
-});
+// bot.onText(/\/listGroups/, async (msg, match) => {
+//   bot.sendMessage(msg.chat.id, await admin.list_group(msg), {
+//     reply_to_message_id: msg.message_id,
+//   });
+// });
 
-bot.onText(/\/listUsers/, async (msg, match) => {
-  bot.sendMessage(msg.chat.id, await admin.list_user(msg), {
-    reply_to_message_id: msg.message_id,
-  });
-});
+// bot.onText(/\/listUsers/, async (msg, match) => {
+//   bot.sendMessage(msg.chat.id, await admin.list_user(msg), {
+//     reply_to_message_id: msg.message_id,
+//   });
+// });
 
-bot.onText(/\/removeGroup/, (msg, match) => {
-  bot.sendMessage(msg.chat.id, admin.remove_group(msg, match), {
-    reply_to_message_id: msg.message_id,
-  });
-});
+// bot.onText(/\/removeGroup/, (msg, match) => {
+//   bot.sendMessage(msg.chat.id, admin.remove_group(msg, match), {
+//     reply_to_message_id: msg.message_id,
+//   });
+// });
 
-bot.onText(/\/admin/, async (msg) => {
-  // await user.add(msg.from);
-  // await user.set_statics(msg.from.id, 1, 3, 5, 0);
-  // console.log( JSON.stringify(await user.statics(msg.from.id)));
-  bot.sendMessage(
-    msg.chat.id,
-    "/addGroup\n/removeGroup\n/bloquear\n/desbloquear\n/listGroups\n/listUsers"
-  );
-});
+// bot.onText(/\/admin/, async (msg) => {
+//   // await user.add(msg.from);
+//   // await user.set_statics(msg.from.id, 1, 3, 5, 0);
+//   // console.log( JSON.stringify(await user.statics(msg.from.id)));
+//   bot.sendMessage(
+//     msg.chat.id,
+//     "/addGroup\n/removeGroup\n/lock\n/unlock\n/listGroups\n/listUsers"
+//   );
+// });
 
 //**                    Game Commands                    */
 
 bot.onText(/\/crear/, async (msg) => {
-  let response = await game.create(msg, false);
+  let response = await game.create(Factory_Request.fromTelegram(msg), false);
   bot.sendMessage(msg.chat.id, response.message, response.options);
 });
 
-bot.onText(/\/limpiar/, async (msg) => {
-  let response = await game.create(msg, true);
+bot.onText(/\/reiniciar/, async (msg) => {
+  let response = await game.create(Factory_Request.fromTelegram(msg), true);
   bot.sendMessage(msg.chat.id, response.message, response.options);
 });
 
 bot.onText(/\/unirse/, async (msg) => {
-  let response = await game.join(msg);
+  let response = await game.join(Factory_Request.fromTelegram(msg));
   bot.sendMessage(msg.chat.id, response.message, response.options);
 });
 
 bot.onText(/\/iniciar/, async (msg) => {
-  let response = await game.join(msg);
+  let response = await game.start(Factory_Request.fromTelegram(msg));
+  bot.sendMessage(msg.chat.id, response.message, response.options);
+});
+
+bot.onText(/\/inicia_ya/, async (msg) => {
+  let response = await game.shuffle(Factory_Request.fromTelegram(msg), false);
   bot.sendMessage(msg.chat.id, response.message, response.options);
 });
 
 bot.onText(/\/estado/, async (msg) => {
-  let response = await game.status(msg);
+  let response = await game.status(Factory_Request.fromTelegram(msg));
   bot.sendMessage(msg.chat.id, response.message, response.options);
 });
 
 bot.onText(/\/configurar/, async (msg) => {
-  let response = await game.config(msg);
+  let response = await game.config(Factory_Request.fromTelegram(msg));
   bot.sendMessage(msg.chat.id, response.message, response.options);
 });
 
 bot.onText(/\/set_(.*) (.*)/, async (msg, match) => {
-  let response = await game.set_settings(msg, match[1], match[2]);
+  let response = await game.set_settings(
+    Factory_Request.fromTelegram(msg),
+    match[1],
+    match[2]
+  );
   bot.sendMessage(msg.chat.id, response.message, response.options);
 });
 
-bot.onText(/\/set_(.*)/, async (msg, match) => {
-  bot.sendMessage(msg.chat.id, resp.set_invalid, response.options);
+bot.onText(/\/set_(.*)/, (msg, match) => {
+  bot.sendMessage(msg.chat.id, resp.set_invalid);
 });
 
 //**                     Set Commands                    */
@@ -168,15 +197,19 @@ bot.setMyCommands([
     description: "Inicia la partida.",
   },
   {
+    command: "inicia_ya",
+    description: "Inicia la partida, pero se salta las configuraciones",
+  },
+  {
     command: "estado",
     description: "Muestra información sobre la partida.",
   },
   {
-    command: "configurar",
-    description: "Muestra el pane de configuración.",
+    command: "reiniciar",
+    description: "Elimina la partida actual y crea una nueva.",
   },
   {
-    command: "limpiar",
-    description: "Elimina la partida actual y crea una nueva.",
+    command: "configurar",
+    description: "Muestra el pane de configuración.",
   },
 ]);
